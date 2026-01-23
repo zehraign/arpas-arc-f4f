d4bed4cbc51ee585b114856fa15872fd585df804
Duaax24 <167181641+Duaax24@users.noreply.github.com>
2026-01-23 01:22:15 +0100

    bilder komprimiert

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -2,8 +2,8 @@ export const quizLocations = [
   {
     id: "kitchen",
     name: "Future Food Kitchen",
-    //coords: { lat: 52.45510590524545, lon: 13.525163019961198 },  
-    coords: { lat: 52.52668739066162, lon: 13.36037664808089 },
+    coords: { lat: 52.540636660690225, lon: 13.391817612557453 },  
+   // coords: { lat: 52.52668739066162, lon: 13.36037664808089 },
     radius: 100, // richtige coords: { lat: 52.45554951375966, lon: 13.52518413859404 }, 
 
     features: {
